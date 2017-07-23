43cf9165fdd11e9d045a169fae05a9ba6e0b318d
AJ Clarke <aj@ajmac.local>
2017-07-23 09:56:31 -0700

    Some more fixes ;)

--- nova-wp-image-resizer.php
+++ nova-wp-image-resizer.php
@@ -2,58 +2,42 @@
 /**
  * Resize images dinamically
  *
- * Usage: nova_resize_thumbnail( get_post_thumbnail_id(), '600x500xcenter-center' );
- *
- * Notes: Width and Height are required, crop is optional
- *        If you input a single number such as 500 it will crop to 500x500
- *
  * @param  string $attach_id
- * @param  string|array $dims
+ * @param  string|array $size
  * @param  bool $retina
  * @return array
+ * @since  1.0
+ *
  */
-function nova_resize_thumbnail( $attach_id, $dims = '', $retina = false ) {
+function nova_resize_thumbnail( $attach_id, $size = '', $retina = false ) {
 	
 	if ( ! $attach_id ) {
 		return;
 	}
 
-	$retina_support = true;
+	// Check retina support
+	$retina_support = false;
 
-	if ( is_array( $dims ) ) {
+	// Check if image size is custom or defined
+	$is_intermediate = false;
 
-		$width  = isset( $dims['width'] ) ? $dims['width'] : '';
-		$height = isset( $dims['height'] ) ? $dims['height'] : '';
-		$crop   = isset( $dims['crop'] ) ? $dims['crop'] : 'center-center';
+	// Size is an array
+	$int_size = isset( $size['size'] ) ? $size['size'] : '';
+	$width    = isset( $size['width'] ) ? $size['width'] : '';
+	$height   = isset( $size['height'] ) ? $size['height'] : '';
+	$crop     = isset( $size['crop'] ) ? $size['crop'] : 'center-center';
 
-	} else {
-
-		$dims = explode( 'x', $dims );
-		if ( isset( $dims[0] ) ) {
-			$count = count( $dims );
-			if ( $count == 3 ) {
-				$width  = $dims[0];
-				$height = $dims[1];
-				$crop   = $dims[2];
-			} elseif ( $count == 2 ) {
-				$width  = $dims[0];
-				$height = $dims[1];
-				$crop   = true;
-			} elseif ( 1 === $count ) {
-				$width  = $dims[0];
-				$height = $dims[0];
-				$crop   = true;
-			} else {
-				return;
-			}
-		}
-
+	// Check if size is intermediate or not
+	if ( $int_size ) {
+		$is_intermediate = true;
 	}
 
+	// Sanitize dimensions
 	$width  = intval( $width );
 	$height = intval( $height );
-	$crop   = esc_html( $crop );
+	$crop   = is_array( $crop ) ? implode( '-', $crop ) : $crop;
 
+	// Get attachment data
 	$src  = wp_get_attachment_image_src( $attach_id, 'full' );
 	$path = get_attached_file( $attach_id );
 
@@ -62,34 +46,9 @@ function nova_resize_thumbnail( $attach_id, $dims = '', $retina = false ) {
 	}
 
 	// Get attachment details
-	$meta         = wp_get_attachment_metadata( $attach_id );
 	$info         = pathinfo( $path );
 	$extension    = '.' . $info['extension'];
 	$path_no_ext  = $info['dirname'] . '/' . $info['filename'];
-	$crop_array   = explode( '-', $crop );
-	$cropped_dims = image_resize_dimensions( $src[1], $src[2], $width, $height, $crop_array );
-
-	// Target image size dims
-	$dst_w = isset( $cropped_dims[4] ) ? $cropped_dims[4] : '';
-	$dst_h = isset( $cropped_dims[5] ) ? $cropped_dims[5] : '';
-
-	// If current image size is smaller or equal to target size return full image
-	if ( empty( $cropped_dims ) || $dst_w > $src[1] || $dst_h > $src[2] ) {
-		
-		if ( $retina ) return; // don't return original for retina images
-
-		return array(
-			'url'    => $src[0],
-			'width'  => $src[1],
-			'height' => $src[2],
-		);
-
-	}
-
-	// Retina can't be cropped to exactly 2x
-	if ( $retina && ( $dst_w !== $width || $dst_h !== $height ) ) {
-		return;
-	}
 
 	// Array of valid crop locations
 	$crop_locations = array(
@@ -105,16 +64,55 @@ function nova_resize_thumbnail( $attach_id, $dims = '', $retina = false ) {
 	);
 
 	// Define crop suffix if custom crop is set and valid
-	$crop_suffix = in_array( $crop, $crop_locations ) ? $crop : '';
+	$crop_suffix = ( is_string( $crop ) && array_key_exists( $crop, $crop_locations ) ) ? $crop : '';
+
+	// Turn crop value into array
+	$crop_array = $crop_suffix ? explode( '-', $crop ) : $crop;
+
+	// Calculate output dimensions after resize
+	$cropped_dims = image_resize_dimensions( $src[1], $src[2], $width, $height, $crop_array );
+
+	// Target image size dims
+	$dst_w = isset( $cropped_dims[4] ) ? $cropped_dims[4] : '';
+	$dst_h = isset( $cropped_dims[5] ) ? $cropped_dims[5] : '';
+
+	// Suffix width and height values
+	$s_width  = $width ? $width : $dst_w;
+	$s_height = $height ? $height : $dst_h;
 
 	// Define suffix
 	if ( $retina ) {
-		$suffix = $width / 2 . 'x' . $height / 2;
+		$suffix = $s_width / 2 . 'x' . $s_height / 2;
 	} else {
-		$suffix = $width . 'x' . $height;
+		$suffix = $s_width . 'x' . $s_height;
 	}
 	$suffix = $crop_suffix ? $suffix . '-' . $crop_suffix : $suffix;
 	$suffix = $retina ? $suffix . '@2x' : $suffix;
+
+	// Define custom intermediate_size based on suffix
+	$int_size = $int_size ? $int_size : 'nova_' . $suffix;
+
+	// If current image size is smaller or equal to target size return full image
+	if ( empty( $cropped_dims )
+		|| $dst_w > $src[1]
+		|| $dst_h > $src[2]
+		|| ( $dst_w == $src[1] && $dst_h == $src[2] )
+	) {
+		
+		if ( $retina ) return; // don't return original for retina images
+
+		return array(
+			'url'    => $src[0],
+			'width'  => $src[1],
+			'height' => $src[2],
+		);
+
+	}
+
+	// Retina can't be cropped to exactly 2x
+	if ( $retina && ( $dst_w !== $width || $dst_h !== $height ) ) {
+		return;
+	}
 
 	// Get cropped path
 	$cropped_path = $path_no_ext . '-' . $suffix . $extension;
@@ -143,9 +141,6 @@ function nova_resize_thumbnail( $attach_id, $dims = '', $retina = false ) {
 
 	}
 
-	// Define intermediate size name
-	$int_size = 'earth_' . $suffix;
-
 	// Crop image
 	$editor = wp_get_image_editor( $path );
 
@@ -171,6 +166,9 @@ function nova_resize_thumbnail( $attach_id, $dims = '', $retina = false ) {
 				$retina_src = nova_resize_thumbnail( $attach_id, $retina_dims, true );
 			}
 
+			// Get thumbnail meta
+			$meta = wp_get_attachment_metadata( $attach_id );
+
 			// Update meta
 			if ( is_array( $meta ) ) {
 
@@ -184,9 +182,12 @@ function nova_resize_thumbnail( $attach_id, $dims = '', $retina = false ) {
 					$mime_type = wp_check_filetype( $cropped_img );
 					$mime_type = isset( $mime_type['type'] ) ? $mime_type['type'] : '';
 
+					// Cropped image file name
+					$dst_filename = $info['filename'] . '-' . $suffix . $extension;
+
 					// Add cropped image to image meta
 					$meta['sizes'][$int_size] = array(
-						'file'      => $new_path,
+						'file'      => $dst_filename,
 						'width'     => $dst_w,
 						'height'    => $dst_h,
 						'mime-type' => $mime_type,
@@ -202,10 +203,11 @@ function nova_resize_thumbnail( $attach_id, $dims = '', $retina = false ) {
 
 			// Return cropped image
 			return array(
-				'url'    => $cropped_img,
-				'width'  => $dst_w,
-				'height' => $dst_h,
-				'retina' => ! empty( $retina_src['url'] ) ? $retina_src['url'] : '',
+				'url'               => $cropped_img,
+				'width'             => $dst_w,
+				'height'            => $dst_h,
+				'retina'            => ! empty( $retina_src['url'] ) ? $retina_src['url'] : '',
+				'intermediate_size' => $int_size,
 			);
 
 		}
